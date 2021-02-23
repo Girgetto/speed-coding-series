918f462b30f4d35b49a40995b5f8c81c80e20767
Giorgio Grassini <giorgiograssini@gmail.com>
2021-02-23 20:35:00 +0100

    refactor

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import { Calculator, ToDoList, Home } from "./components";
 
-function App() {
+function Routes() {
   return (
     <div className="App">
       <Router>
@@ -26,4 +26,4 @@ function App() {
   );
 }
 
-export default App;
+export default Routes;
